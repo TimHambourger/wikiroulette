d11af4113c346cd7628d6b2d808452652a346c14
tim_hambourger <thambourger@osintegrators.com>
2014-03-23 08:34:17 -0400

    Add search functionality, loading icon, styling.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var doButtonClick = function() {
+(function() {
 
 //API base, without language subdomain or query string
 var API_BASE = "wikipedia.org/w/api.php?";
@@ -15,6 +15,32 @@ var LANGUAGES = {
     Italian: 'it'
 };
 
+var GENERATORS = {
+    random: "random",
+    search: "search",
+    category: "category"
+};
+
+//place to store jQuery objects
+var DOM = {
+    loading: null,
+    searchInput: null,
+    searchForm: null
+};
+
+var searchParams = {
+    articles: null,
+    language: null,
+    generator: null
+};
+
+//mapping to jQuery objects
+var selectedItems = {
+    articles: null,
+    language: null,
+    searchby: null
+};
+
 //number of characters to show in an article by default
 var CHARS_TO_SHOW = 200;
 
@@ -23,65 +49,65 @@ var CHARS_TO_SHOW = 200;
  * @param n number of articles to request. Between 1 and 10 inclusive.
  * @param language string code for wikipedia subdomain. 'en' for English.
  */
-function requestArticles(n, language) {
-    console.log("requestArticles");
-    if (n !== parseInt(n) || n < 1 || n > 10) {
-	console.error("index.js requestArticles: bad article number");
+function requestArticles(n, language, generator) {
+    //if don't have a search term, just do random search
+    if (!DOM.searchInput || !DOM.searchInput.val())
+	generator = GENERATORS.random;
+    var urlBase = "http://"+language+"."+API_BASE;
+    var data = {
+	action: "query",
+	prop: "extracts",
+	format: "json",
+	exintro: "",
+	exlimit: n,
+	generator: generator,
+	callback: "?"
+    };
+    if (generator === GENERATORS.random) {
+	data.grnnamespace = 0;
+	data.grnlimit = n;
+    } else if (generator === GENERATORS.search) {
+	data.gsrsearch = DOM.searchInput.val();
+	data.gsrnamespace = 0;
+	data.gsrlimit = n;
     }
-    var queryString = ["action=query&prop=extracts&format=json&exintro=&generator=random&grnnamespace=0&callback=?&", "exlimit=", n, "&grnlimit=", n].join("");
-    var url = ["http://", language, ".", API_BASE, queryString].join("");
-    //var url = "http://"+language+"."+API_BASE;
-    console.info("index.js ajax request "+url);
-    //console.info("index.js ajax request origin "+location.origin);
-    /*$.ajax({
-	url: url,
-	data: {
-            action: "query",
-	    prop: "extracts",
-	    format: "json",
-	    exlimit: n,
-	    exintro: "",
-	    generator: "random",
-	    grnnamespace: 0,
-	    grnlimit: n,
-	    origin: location.origin
-	},
-	success: curryHttpSuccess(language),
-	error: httpError
-    });*/
-    var timeoutId = setTimeout(httpTimeout, 2000);
-    $.getJSON(url, curryJSONPSuccess(language, timeoutId));
-}
-
-/*function httpError(xhr, status) {
-    console.error("index.js "+moment().format("HH:mm:ss")+" requestArticles error "+status);
-}
-
-function curryHttpSuccess(language) {
-    return function(data) {
-	console.log("index.js "+moment().format("HH:mm:ss")+" requestArticles success "+data);
-	try {
-	    data = JSON.parse(data);
-	    var extracts = data.query.pages;
-	    addExtracts(extracts, language);
-	} catch (e) {
-	    console.error("JSON parse error");
+    console.log(DOM.searchInput.val());
+    console.log(JSON.stringify(data));
+    var urlBits = [urlBase];
+    for (var key in data) {
+	if (data.hasOwnProperty(key)) {
+	    urlBits.push(key+"="+data[key]+"&");
 	}
     }
-}*/
+    var url = urlBits.join("");
+    //var queryString = ["action=query&prop=extracts&format=json&exintro=&generator=random&grnnamespace=0&callback=?&", "exlimit=", n, "&grnlimit=", n].join("");
+
+//http://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exlimit=1&exintro=&generator=search&gsrsearch=cross&gsrnamespace=0&gsrlimit=1
+    console.log("url: "+url);
+    var timeoutId = setTimeout(httpTimeout, 2000);
+    showLoading();
+    $.getJSON(url, curryJSONPSuccess(language, timeoutId)
+    );
+}
 
 function httpTimeout() {
+    hideLoading();
+    alert("Request timeout. Please check your internet connection and try again.");
     console.error("index.js ajax timeout");
 }
 
 function curryJSONPSuccess(language, timeoutId) {
     return function(data) {
 	clearTimeout(timeoutId);
-	console.info("index.js "+moment().format("HH:mm:ss")+" JSONP success");
-	console.log(JSON.stringify(data));
-	var extracts = data.query.pages;
-	addExtracts(extracts, language);
-	console.log("check1");
+	try {
+	    console.info("index.js "+moment().format("HH:mm:ss")+" JSONP success");
+	    var extracts = data.query.pages;
+	    addExtracts(extracts, language);
+	    hideLoading();
+	} catch (e) {
+	    //if error, processing request, we'll just throw same error alert
+	    httpTimeout();
+	}
     };
 }
 
@@ -98,11 +124,21 @@ Handlebars.registerHelper("show", function(params, options) {
 	if (extracts.hasOwnProperty(key)) {
 	    var extract = extracts[key];
 	    var title = extract.title;
-	    var url = ["http://", language, ".", WIKIPEDIA_BASE, encodeURIComponent(title)].join("");
+	    var url =
+		["http://", language, ".", WIKIPEDIA_BASE,
+		       encodeURIComponent(title)].join("");
+	    var content = extract.extract;
+	    //get first paragraph only
+	    var match = content.match(/^.*?<p>.*?<\/p>/);
+	    var firstPara;
+	    if (match) {
+		firstPara = match[0];
+	    }
+
 	    var compiledExtract = options.fn({
 		url: url,
 		title: title,
-		extract: extract.extract
+		extract: firstPara
 	    });
 	    out.push(compiledExtract);
 	}
@@ -121,14 +157,128 @@ function addExtracts(extracts, language) {
 	}
     };
     var compiledExtracts = extractsTemplate(context);
-    console.log(compiledExtracts);
     $("#extracts-div").html(compiledExtracts);
-    console.log("check2");
 }
 
-return function() {
-    console.log("buttonClick");
-    requestArticles(5, LANGUAGES.English);
+function showLoading() {
+    if (DOM.loading) DOM.loading.css("display", "block");
 }
 
-}();
+function hideLoading() {
+    if (DOM.loading) DOM.loading.css("display", "none");
+}
+
+function showSearchInput() {
+    if (DOM.searchInput) {
+	DOM.searchInput.val("");
+	DOM.searchForm.css("display", "block");
+    }
+}
+
+function hideSearchInput() {
+    if (DOM.searchInput) {
+	DOM.searchForm.css("display", "none");
+    }
+}
+	
+
+function doSearch() {
+    requestArticles(searchParams.articles,
+		    searchParams.language, searchParams.generator);
+}
+
+$(".submit-btn").on("click", doSearch);
+
+$(document).on("click", ".menu-language li a", function(e) {
+    e.preventDefault();
+    var target = $(e.target);
+    var value = selectLanguage(target);
+    $("#language-btn-text").html(value);
+});
+
+/* perform select for language
+ *
+ * @param language jQuery object <a> element selected
+ * @return value of element selected
+ */
+function selectLanguage(target) {
+    changeSelection("language", target);
+    var key = target.data("key");
+    var value = target.html();
+    searchParams.language = LANGUAGES[key];
+    return value;
+}
+
+$(document).on("click", ".menu-articles li a", function(e) {
+    e.preventDefault();
+    var target = $(e.target);
+    var value = selectArticles(target);
+    $("#articles-btn-text").html(value);
+});
+
+/* perform selection for number of articles
+ *
+ * @param target jQuery object <a> element selected
+ * @return text of element selected
+ */
+function selectArticles(target) {
+    changeSelection("articles", target);
+    var value = target.html();
+    searchParams.articles = value;
+    return value;
+}
+
+$(document).on("click", ".menu-searchby li a", function(e) {
+    e.preventDefault();
+    var target = $(e.target);
+    var value = selectSearchBy(target);
+    $("#searchby-btn-text").html(value);
+});
+
+
+/* perform select for search by
+ *
+ * @param language jQuery object <a> element selected
+ * @return value of element selected
+ */
+function selectSearchBy(target) {
+    changeSelection("searchby", target);
+    var key = target.data("key");
+    var value = target.html();
+    var generator = GENERATORS[key];
+    if (generator === GENERATORS.search || generator === GENERATORS.category) {
+	showSearchInput();
+    } else {
+	hideSearchInput();
+    }
+    searchParams.generator = generator;
+    console.log("set generator to "+searchParams.generator);
+    return value;
+}
+
+/* Change bg for menu items
+ * 
+ * @param menuType string type of menu, e.g. "articles", "language"
+ * @param target jQuery object target of button click
+ */
+function changeSelection(menuType, target) {
+    if (selectedItems[menuType])
+	selectedItems[menuType].removeClass("bg-primary");
+    selectedItems[menuType] = target;
+    target.addClass("bg-primary");
+}
+
+$(document).ready(function() {
+    DOM.loading = $("#loading");
+    DOM.searchInput = $("#search-input");
+    DOM.searchForm = $("#search-form");
+    var defaultLanguageSelection = $(".menu-language a.default-selection");
+    var defaultArticlesSelection = $(".menu-articles a.default-selection");
+    var defaultSearchbySelection = $(".menu-searchby a.default-selection");
+    selectLanguage(defaultLanguageSelection);
+    selectArticles(defaultArticlesSelection);
+    selectSearchBy(defaultSearchbySelection);
+    doSearch();
+});
+
+})();
